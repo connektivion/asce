30265af7d907c87de873be2afc2d3a604ddfb449
connektivion <connektivion@gmail.com>
2016-01-10 19:36:22 +0530

    Nagarjuna Red Hat configuration changes

--- db/db.js
+++ db/db.js
@@ -60,7 +60,7 @@ var Gallery = mongoose.model('GallerySchema',GallerySchema);
 var RecentPosts = mongoose.model('RecentPostsSchema',RecentPostsSchema);
 var Users = mongoose.model('UserSchema',UserSchema);
 
-mongoose.connect( "mongodb://arpan:qwerty12345@ds043398.mongolab.com:43398/asce" , function (err) {
+mongoose.connect( "mongodb://localhost/asce" , function (err) {
         if(err) console.log(err);
     });
 
